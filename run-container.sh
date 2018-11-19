78b80905f2f8546177f6ee7e0775c14e46ec702e
Benoit Moussaud <bmoussaud@xebialabs.com>
2018-11-19 19:10:40 +0100

    refactoring

--- run-container.sh
+++ run-container.sh
@@ -1,1 +1,2 @@
-    docker run -p 4516:4516 -e ADMIN_PASSWORD=admin -e JDBC_USERNAME=postgres -e JDBC_PASSWORD=mysecretpassword -e JDBC_URL=jdbc:postgresql://192.168.99.102:30460/postgres -e JDBC_DRIVER=org.postgresql.Driver bmoussaud/xl-deploy-with-db:8.2.1
+#!/usr/bin/env bash
+docker run -p 4516:4516 -e ADMIN_PASSWORD=admin -e JDBC_USERNAME=postgresadmin1 -e JDBC_PASSWORD=xl123admin456 -e JDBC_URL=jdbc:postgresql://xldbpostgres.cjofzmugihxg.us-west-2.rds.amazonaws.com:5432/postgres -e JDBC_DRIVER=org.postgresql.Driver bmoussaud/xl-deploy-with-db:8.2.2
