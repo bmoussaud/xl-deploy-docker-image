28017c448bd9746301ef56c8d8657aaf81d65770
Benoit Moussaud <bmoussaud@xebialabs.com>
2018-11-19 17:14:44 +0100

    upgrade xld-kubernetes-plugin-8.5.0-rc.1.xldp

--- build_image.sh
+++ build_image.sh
@@ -1,1 +1,1 @@
-docker build --build-arg XLD_VERSION=8.2.1 --tag bmoussaud/xl-deploy-with-db:8.2.1  -f debian-slim/Dockerfile .
+docker build --build-arg XLD_VERSION=8.2.1 --tag bmoussaud/xl-deploy-with-db:8.2.2  -f debian-slim/Dockerfile .
